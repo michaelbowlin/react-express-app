4096c825781dfda527eda292335507ab5860a72d
info@michaelbowlin.com <keplerdog!>
2016-03-14 18:00:29 -0600

    Created three new methods to the GroceryListAddItems.jsx. Currently working in the console.

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -1,12 +1,25 @@
 var React = require('react/addons');
 
 module.exports = React.createClass({
-    render: function() {
+    getInitialState: function () {
+        // currently only a read item
+        // to set the write item we have to set the onChange property
+        return {input: ""};
+    },
+    handleInputName: function (e) {
+        // NEVER: change the state directly.. use setState instead
+        this.setState({input: e.target.value});
+    },
+    addItem: function (e) {
+        e.preventDefault();
+        console.log("Adding Item! ", this.state.input)
+    },
+    render: function () {
         return (
             <div className='grocery-addItem'>
-                <form action="">
-                    <input type="text"/>
-                    <button> Add Item </button>
+                <form onSubmit={this.addItem}>
+                    <input value={this.state.input} onChange={this.handleInputName}/>
+                    <button> Add Item</button>
                 </form>
             </div>
         )

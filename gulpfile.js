671b60608f8a2e04f2fb08b1b2c744135d977ac0
info@michaelbowlin.com <keplerdog!>
2016-03-17 13:58:22 -0600

    Finished set up for tracuer and babel feeding into a dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,12 @@ var browserify = require('browserify');
 // a format that can be used by a static server like express
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
-
+//ES6 additions (http://weblogs.asp.net/dwahlin/getting-started-with-es6-%E2%80%93-transpiling-es6-to-es5)
+var traceur = require('gulp-traceur');
+var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+var es6Path = 'app/es6/*.js';
+var compilePath = 'dist/compiled-es6';
 
 gulp.task('live-server', function () {
     var server = new LiveServer('server/main.js');
@@ -36,10 +41,35 @@ gulp.task('copy', function () {
     .pipe(gulp.dest('./.tmp'));
 });
 
+// This adds the plumber module into the streaming process to handle any errors that occur in the the piping process more gracefully
+// and then invokes the Traceur transpiler. The blockBinding property allows block level definitions to be used in the
+// ES6 code via a new let keyword
+gulp.task('traceur', function () {
+    gulp.src([es6Path])
+        .pipe(plumber())
+        .pipe(traceur({ blockBinding: true }))
+        .pipe(gulp.dest(compilePath + '/traceur'));
+});
+
+// cause ES6 to be transpiled to ES5 (using two different techniques) but they won’t run any time an ES6 file is saved
+gulp.task('babel', function () {
+    gulp.src([es6Path])
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest(compilePath + '/babel'));
+});
+
+// Automation
+gulp.task('watch', function() {
+    gulp.watch([es6Path], ['traceur', 'babel']);
+});
+
 // Dependent upon our live-server and bundle task
-gulp.task('serve', ['bundle', 'live-server'], function () {
+gulp.task('serve', ['bundle', 'live-server','traceur', 'babel', 'watch'], function () {
     browserSync.init(null, { // null means server already going
         proxy: "http://localhost:7777",
         port: 9001
     })
 });
+
+//gulp.task('default', ['traceur', 'babel', 'watch']);
